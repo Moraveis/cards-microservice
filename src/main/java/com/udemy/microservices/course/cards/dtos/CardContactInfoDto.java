6b6d5d7f3f40e254b1f5f89a8d5ac2aac85c1424
Joao Vitor <joaovitormoraveis@gmail.com>
2024-04-10 21:18:48 -0400

    fix: component to hold configuration values

--- src/main/java/com/udemy/microservices/course/cards/dtos/CardContactInfoDto.java
+++ src/main/java/com/udemy/microservices/course/cards/dtos/CardContactInfoDto.java
@@ -3,12 +3,13 @@ package com.udemy.microservices.course.cards.dtos;
 import lombok.*;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Map;
 
 @Data
-@Configuration
+@Component
 @ConfigurationProperties(prefix = "cards")
 public class CardContactInfoDto {
 
